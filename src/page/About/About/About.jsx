a75aa0393fd129b638a9e3e4c0656a402584eab6
ImranRH321 <imranrh247@gmail.com>
2023-12-23 15:05:52 +0600

    some about ds

--- src/page/About/About/About.jsx
+++ src/page/About/About/About.jsx
@@ -4,20 +4,21 @@ import { FaMusic } from 'react-icons/fa';
 import man1 from '../../../assets/about/man1.png'
 import man2 from '../../../assets/about/man2.png'
 import CommonSectionTitle from '../../share/CommonSectionTitle/CommonSectionTitle';
+import OurMeet from '../OurMeet/OurMeet';
 
 const About = () => {
     return (
         <div>
             <h1 className='text-black'>Iabout page </h1>
             {/* <CoverBackground></CoverBackground> */}
-            <div className="container_flow">
+            <div className="container_flow part1">
                 <CommonSectionTitle sectionTitle={"Our Misssion"}></CommonSectionTitle>
                 <h1 className='text-3xl font-extrabold text-left my-3'>Your happiness is our <br /> first priority</h1>
                 {/* row bro */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-3">
-                    <div className='border border-red-400'>
-                        
-                        <div className="border flex justify-center gap-5 items-center text-left">
+                    <div className=' border-red-400'>
+
+                        <div className=" flex justify-center gap-5 items-center text-left">
                             <div className="avatar ">
                                 <div className="w-12">
                                     <img className='rounded-full' src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -28,7 +29,7 @@ const About = () => {
                                 <p>Mollis facilisis nibh libero in nisl. Pulvinar donec lorem arcu augue feugiat enim.</p>
                             </div>
                         </div>
-                        <div className="border flex my-5 justify-center gap-5 items-center text-left">
+                        <div className=" flex my-5 justify-center gap-5 items-center text-left">
                             <div className="avatar ">
                                 <div className="w-12">
                                     <img className='rounded-full' src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -39,7 +40,7 @@ const About = () => {
                                 <p>Mollis facilisis nibh libero in nisl. Pulvinar donec lorem arcu augue feugiat enim.</p>
                             </div>
                         </div>
-                        <div className="border flex justify-center gap-5 items-center text-left">
+                        <div className=" flex justify-center gap-5 items-center text-left">
                             <div className="avatar ">
                                 <div className="w-12">
                                     <img className='rounded-full' src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -52,8 +53,45 @@ const About = () => {
                         </div>
                     </div>
                     <div className='Right_img border flex  gap-5 flex-wrap'>
-                        <img className='w-[200px]' src={man1} alt="noImg" />
-                        <img className='w-[200px]' src={man2} alt="noImg" />
+                        <img className='w-[200px] rounded' src={man1} alt="noImg" />
+                        <img className='w-[200px] rounded' src={man2} alt="noImg" />
+                    </div>
+                </div>
+            </div>
+            <div className="part2 OurMeetPage">
+                <OurMeet></OurMeet>
+            </div>
+            {/* Start bro */}
+            <div className="my-5 bosStart">
+                <h1 className='text-center font-extrabold text-3xl'>How we’ve helped our clients</h1>
+                <div className="heloStart flex gap-10 justify-center flex-wrap mt-5">
+                    <div className="stats shadow">
+                        <div className="stat">
+                            <div className="stat-value text-[#377DFF]">15</div>
+                            <div className="stat-desc font-bold mt-3">Years of experience</div>
+                        </div>
+
+                    </div>
+                    <div className="stats shadow">
+                        <div className="stat">
+                            <div className="stat-value text-[#377DFF]">36k</div>
+                            <div className="stat-desc font-bold mt-3">Social followers</div>
+                        </div>
+
+                    </div>
+                    <div className="stats shadow">
+                        <div className="stat">
+                            <div className="stat-value text-[#377DFF]">428</div>
+                            <div className="stat-desc font-bold mt-3">Project completed</div>
+                        </div>
+
+                    </div>
+                    <div className="stats shadow">
+                        <div className="stat">
+                            <div className="stat-value text-[#377DFF]">92k</div>
+                            <div className="stat-desc font-bold mt-3">Social profiles</div>
+                        </div>
+
                     </div>
                 </div>
             </div>
---
+++ src/page/About/OurMeet/OurMeet.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import CommonSectionTitle from '../../share/CommonSectionTitle/CommonSectionTitle';
+import p1 from '../../../assets/ourMeet/p1.png'
+import p2 from '../../../assets/ourMeet/p2.png'
+import p3 from '../../../assets/ourMeet/p3.png'
+import p4 from '../../../assets/ourMeet/p4.png'
+
+const OurMeet = () => {
+    return (
+        <div>
+            <div className='flex justify-center mt-10'> <CommonSectionTitle sectionTitle={'Our Team'}></CommonSectionTitle></div>
+            <h1 className='text-3xl font-extrabold'>Meet our creative team</h1>
+            <br />
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 text-lef mt-5">
+                <div className="card bg-base-100 shadow-xl">
+                    <div className=" text-left">
+                        <img src={p1} alt="noImg" />
+                        <p className='font-extrabold text-center mt-3'>Faye Carson</p>
+                        <p className=' text-center'>Instuctor</p>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className=" text-left">
+                        <img src={p2} alt="noImg" />
+                        <p className='font-extrabold text-center mt-3'>Faye Carson</p>
+                        <p className=' text-center'>Instuctor</p>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className=" text-left">
+                        <img src={p3} alt="noImg" />
+                        <p className='font-extrabold text-center mt-3'>Faye Carson</p>
+                        <p className=' text-center'>Instuctor</p>
+                    </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl">
+                    <div className=" text-left">
+                        <img src={p4} alt="noImg" />
+                        <p className='font-extrabold text-center mt-3'>Faye Carson</p>
+                        <p className=' text-center'>Instuctor</p>
+                    </div>
+                </div>
+            </div>
+                <p className='text-center mt-3'>... .... .... </p>
+        </div>
+
+    );
+};
+
+export default OurMeet; 
